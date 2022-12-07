61de463077062de0452e904e4d2d1c958eff6aee
Tomas Walander <twalander@gmail.com>
2022-12-07 10:20:12 +0100

    Separate publish and validate of apim publish command.

--- clis/ado-cli/src/commands/apim/publish.ts
+++ clis/ado-cli/src/commands/apim/publish.ts
@@ -16,7 +16,6 @@ const logger = createLogger();
 /***********/
 
 export type ApimPublishCommandOptions = {
-  mode: 'validate' | 'apply';
   url?: string;
   resourceGroupName: string;
   apiManagementName: string;
@@ -31,13 +30,12 @@ const readApiConfig = async (path: string): Promise<ApiConfig[]> => {
   return JSON.parse(content);
 };
 
-export const validateOrPublishAction = async (
+export const validateAction = async (
   opts: ApimPublishCommandOptions,
   command: Command,
 ): Promise<void> => {
   logger.debug('Initialised with options', opts);
-  const { mode, apiConfigPath, subscriptionId, url, ...rest } = opts;
-  const shouldApply = mode === 'apply';
+  const { apiConfigPath, subscriptionId, url } = opts;
 
   const printError = (msg: string, details?: unknown) => {
     logger.breakline();
@@ -61,13 +59,53 @@ export const validateOrPublishAction = async (
     return;
   }
 
-  const result = await axios.get<OpenAPIV2.Document>(url);
-  const openApiSpec = result.data;
-  logger.debug('Fetched OpenApi specification', {
-    url,
-    headers: result.headers,
-    data: openApiSpec,
-  });
+  let apiConfigs: ApiConfig[] | undefined;
+  try {
+    apiConfigs = await readApiConfig(apiConfigPath);
+  } catch (err) {
+    return printError(`Failed reading the api-config with error: ${err}.`);
+  }
+  const error = validateApiConfigs(apiConfigs);
+  if (error) {
+    return printError(
+      `Api Config at "${apiConfigPath}" is not valid. Message: '${error.message}'.`,
+      { error },
+    );
+  }
+  // TODO figure out how to validate more stuff - but reliably
+  // I.e., what to do if it's not possible to fetch the openapi spec at this point
+  logger.info(`Looks ok to me!`);
+  logger.warn(`Currently the script is only validating the api-config file.`);
+};
+
+export const publishAction = async (
+  opts: ApimPublishCommandOptions,
+  command: Command,
+): Promise<void> => {
+  logger.debug('Initialised with options', opts);
+  const { apiConfigPath, subscriptionId, url, ...rest } = opts;
+
+  const printError = (msg: string, details?: unknown) => {
+    logger.breakline();
+    logger.err(msg, details);
+    logger.breakline();
+    command.help({ error: true });
+  };
+
+  if (!subscriptionId) {
+    printError('An Azure Subscription Id must be provided.');
+    return;
+  }
+  if (!url) {
+    printError(
+      'An Open Api Spec must be provided as a publicly available url.',
+    );
+    return;
+  }
+  if (!apiConfigPath || !apiConfigPath.endsWith('.json')) {
+    printError('The api-config must be a path to a json file.');
+    return;
+  }
 
   let apiConfigs: ApiConfig[] | undefined;
   try {
@@ -82,15 +120,40 @@ export const validateOrPublishAction = async (
       { error },
     );
   }
+
+  let openApiSpec: OpenAPIV2.Document | undefined;
+  try {
+    const result = await axios.get<OpenAPIV2.Document>(url);
+    openApiSpec = result.data;
+    logger.debug('Fetched OpenApi specification', {
+      url,
+      headers: result.headers,
+      data: openApiSpec,
+    });
+  } catch (err: unknown) {
+    printError(
+      `Failed fetching the OpenApi spec. Please ensure the open api spec is a publicly available url.`,
+      { err, url },
+    );
+  }
+
   await Promise.all(
     apiConfigs.map(api =>
       publishApiFromSpec({
         ...rest,
         ...api,
-        apply: shouldApply,
+        apply: true,
         subscriptionId,
         openApiSpec: openApiSpec as OpenAPIV2.Document,
       }),
     ),
   );
 };
+
+export const validateOrPublishAction = async (
+  opts: ApimPublishCommandOptions & { mode: 'validate' | 'apply' },
+  command: Command,
+): Promise<void> =>
+  opts.mode === 'apply'
+    ? publishAction(opts, command)
+    : validateAction(opts, command);
